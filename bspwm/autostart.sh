f6beb130f91ff3c10736e221cd5e95dbb64c7803
lernaeus <114980450+lernaeus-ton@users.noreply.github.com>
2022-10-04 10:04:06 +0300

    things

---
+++ bspwm/autostart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Start discord (minimized in the tray)
+discord --ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy --start-minimized &
+
+
