b302c76eee93de4d042852d9389b0926ea4c85e4
lernaeus-ton <114980450+lernaeus-ton@users.noreply.github.com>
2022-10-13 12:54:38 +0300

    icon in weather polybar

--- polybar/weather.sh
+++ polybar/weather.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-#get_icon() {
-#    case $1 in
-#        # Icons for weather-icons
-#        01d) icon="";;
-#        01n) icon="";;
-#        02d) icon="";;
-#        02n) icon="";;
-#        03*) icon="";;
-#        04*) icon="";;
-#        09d) icon="";;
-#        09n) icon="";;
-#        10d) icon="";;
-#        10n) icon="";;
-#        11d) icon="";;
-#        11n) icon="";;
-#        13d) icon="";;
-#        13n) icon="";;
-#        50d) icon="";;
-#        50n) icon="";;
-#        *) icon="";
+get_icon() {
+    case $1 in
+        # Icons for weather-icons
+        01d) icon="";;
+        01n) icon="";;
+        02d) icon="";;
+        02n) icon="";;
+        03*) icon="";;
+        04*) icon="";;
+        09d) icon="";;
+        09n) icon="";;
+        10d) icon="";;
+        10n) icon="";;
+        11d) icon="";;
+        11n) icon="";;
+        13d) icon="";;
+        13n) icon="";;
+        50d) icon="";;
+        50n) icon="";;
+        *) icon="摒";
 #
 #        # Icons for Font Awesome 5 Pro
 #        #01d) icon="";;
@@ -36,10 +36,10 @@
 #        #13*) icon="";;
 #        #50*) icon="";;
 #        #*) icon="";
-#    esac
-#
-#    echo $icon
-#}
+    esac
+
+    echo $icon
+}
 
 KEY=""
 CITY=""
@@ -72,5 +72,5 @@ if [ -n "$weather" ]; then
     weather_temp=$(echo "$weather" | jq ".main.temp" | cut -d "." -f 1)
     weather_icon=$(echo "$weather" | jq -r ".weather[0].icon")
 
-    echo "${weather_desc^}", "$weather_temp$SYMBOL"
+    echo "$(get_icon "$weather_icon") " "${weather_desc^}", "$weather_temp$SYMBOL"
 fi
